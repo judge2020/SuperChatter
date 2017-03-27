ff18760185080d218f311ab19e51d52bfeb3c28a
judge2020 <hunterray55@hotmail.com>
2017-03-27 15:09:39 -0400

    base

---
+++ SuperChatter/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperChatter
+{
+	public class Config
+	{
+		#region settings
+
+		private static Config _config;
+
+		[DefaultValue(2000)]
+		public int IntervalMillisends = 2000;
+
+
+
+
+		#endregion
+	}
+}
---
+++ SuperChatter/Core/CoreLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace SuperChatter.Core
+{
+	class CoreLoop
+	{
+		private static CoreLoop _instance;
+
+		public static CoreLoop Instance => _instance ?? (_instance = new CoreLoop());
+
+		public DispatcherTimer Timer = new DispatcherTimer();
+
+		public CoreLoop()
+		{
+			
+		}
+	}
+}
